dd2670844d602a28ebea2b4e951fb0baa0c5af6b
Zuzana <z.kertysova@gmail.com>
2025-10-15 17:56:56 +0200

    refining_calculations

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
+from calculation import simulate_creatine
 
-st.title('blabla')
+st.title('Creatine Saturation Calculator')
 
 if "weight_key" not in st.session_state:
     st.session_state.weight_key = 70
 
 weight = st.slider('Weight in kg', 45, 100, key="weight_key")
 height = st.slider('Height in cm', 145, 215, key="height_key")
-
 
 st.write(st.session_state.weight_key)
--- calculation.py
+++ calculation.py
@@ -1,1 +1,17 @@
-# Here, there will be calculations
+# Script for calculation/simulation
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
+import plotly.graph_objects as go
+import pandas as pd
+
+def simulate_creatine():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
